119e8aaf67015f6997c6715ad73496258b6d6b64
rares vasiliu <rv.vasiliu@gmail.com>
2025-11-03 11:56:17 +0100

    chore: update sematic ver

--- release.config.mjs
+++
@@ -1,8 +0,0 @@
-import config from 'semantic-release-preconfigured-conventional-commits' with { type: 'json' }
-
-config.plugins.push(
-    '@semantic-release/github',
-    '@semantic-release/git',
-)
-
-export default config
