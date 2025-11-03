52ce9c1181f3a122c94a2da0f67693001c040edd
rares vasiliu <rv.vasiliu@gmail.com>
2025-11-03 12:30:30 +0100

    chore: no commits pls

--- release.config.mjs
+++ release.config.mjs
@@ -2,7 +2,6 @@ import config from 'semantic-release-preconfigured-conventional-commits' with { 
 
 config.plugins.push(
     '@semantic-release/github',
-    '@semantic-release/git',
 )
 
 export default config
